69f4f30eb4d7ddc03b11a6429fc9033f888df15f
Mohammed Salem - PC1 <mohammed.salem9023@outlook.com>
2021-01-17 14:38:37 +0200

    new form added

--- WpfApp1/frmHome.xaml.cs
+++ WpfApp1/frmHome.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for frmHome.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class frmHome : Window
     {
-        public MainWindow()
+        public frmHome()
         {
             InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            frmHome x = new frmHome();
-            x.ShowDialog();
-            
         }
     }
 }
